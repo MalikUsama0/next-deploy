17b1b3341bc53b5f7f1c0a1c9a0777014f4bf314
usama_nusrat <malik.usama@iplex.co>
2023-09-14 22:16:01 +0500

    nextttt

---
+++ src/app/login/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useState } from "react";
+export default function Login() {
+  const [name, seName] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = () => {
+    let userName = "usama";
+    let pass = "usama123";
+    if (name == userName && password == pass) {
+      alert("successfully login");
+    } else {
+      alert("login failed");
+    }
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <input
+        type="text"
+        value={name}
+        placeholder="enter user name"
+        style={{ padding: "8px", margin: "10px 0" }}
+        onChange={(e) => seName(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="enter password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        style={{ padding: "8px", margin: "10px 0" }}
+      />
+      <button
+        style={{
+          padding: "10px",
+          background: "blue",
+          color: "white",
+          border: "none",
+          borderRadius: "3px",
+        }}
+        onClick={handleLogin}
+      >
+        Login
+      </button>
+    </div>
+  );
+}
